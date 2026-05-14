11de580b4a0c05638bd95c1224e2d39cdf46bdf1
John Andrews <torrental@gmail.com>
2026-05-14 14:15:28 -0400

    Update index.ts to include cfAccessTeamName for authentication

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { R2Explorer } from "r2-explorer";
 export default R2Explorer({
 	// Set to false to allow users to upload files
 	readonly: true,
+	cfAccessTeamName: 'swearnet' 
 
 	// Learn more how to secure your R2 Explorer instance:
 	// https://r2explorer.com/getting-started/security/
